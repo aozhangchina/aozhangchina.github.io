23b251c4e044636dc9e557928f29599858a22925
aozhangchina <zhangao7@qq.com>
2021-06-07 12:59:38 -0500

    Add files via upload

--- R/GCA_SCATool/GCA_SCATool.r
+++ R/GCA_SCATool/GCA_SCATool.r
@@ -9,4 +9,4 @@ GCASCAFile <- GBIT.readFile(header = T, choose = T)
 rownames(GCASCAFile) <- demoGCA_one_rows <- GCASCAFile[,1]
 GCASCAFile <- GCASCAFile[,-1]
 
-GCA_SCA <- GBIT.get.GCASCA(GCASCAFile,output_f = F)
+GCA_SCA <- GBIT.get.GCASCA(GCASCAFile,output_f = output_f)
