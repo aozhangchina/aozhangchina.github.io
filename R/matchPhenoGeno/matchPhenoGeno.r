199f2e7f49c4dc906d1bbe71cacc21f7e055fa28
aozhangchina <zhangao7@qq.com>
2021-06-20 13:10:53 -0500

    Add files via upload

--- R/matchPhenoGeno/matchPhenoGeno.r
+++ R/matchPhenoGeno/matchPhenoGeno.r
@@ -1,6 +1,6 @@
 source("https://dataholdcn.cn/R/GBIT/GBIT.R")
 if(!exists("fName.geno")){fName.geno <- "myGeno"}
-if(!exists("fName.pheno")){fName.Pheno <- "myPheno"}
+if(!exists("fName.pheno")){fName.pheno <- "myPheno"}
 if(!exists("ID")){ID <- "GenoID"}
 if(!exists("subtext")){subtext <- ":.*|-.*"}
 
@@ -27,5 +27,5 @@ cat("Pheno: ",setdiff(myPhenoClean$GeneID,all_material),".\n")
 myGeno <- cbind(myGeno_head,myGenoHeaderClean[,all_material])
 myPheno <- myPhenoClean[-which(myPhenoClean$GeneID %in% setdiff(myPhenoClean$GeneID,all_material)),]
 
-write.table(myPheno,paste0(fName.Pheno,".csv"),sep = ",",quote = F,col.names = T,row.names = F)
+write.table(myPheno,paste0(fName.pheno,".csv"),sep = ",",quote = F,col.names = T,row.names = F)
 GBIT.writeHMP(wData = myGeno,fName = paste0(fName.geno,"_",(ncol(myGeno)-11),"_",nrow(myGeno)))
