02ff792200ec711b10f12a98aa1f18446b992441
aozhangchina <zhangao7@qq.com>
2022-03-30 16:12:00 +0800

    Add files via upload

--- R/HMPDeleteDup/HMPDeleteDup.md
+++ R/HMPDeleteDup/HMPDeleteDup.md
@@ -10,16 +10,23 @@
 
 ## 最新更新
 
+【20220330 15:12:30】
+
+1. 整合了计算重复性功能。
+2. 增加了两个数据合并的功能，但很耗时。
+
 【2021-06-19 20:21:44】
 
-功能完成。
+1. 功能完成。
 
 ## 功能
 
+测序时，可能会设置相同的材料测定两次，以检测测序的稳定性。这些材料会对分析产生影响，需要先进行处理。
+
 1. HMP基因型文件，长文件名的缩短，如：GBPSVD22:2-B-B-3缩短为GBPSVD22<可选>。
-2. 找到重复的材料名，剔除其中一个。
+2. 找到重复的材料名，剔除其中一个或将两个材料的结果合并成一个（很耗时）。
 3. 自定义新的HMP文件名，自动补全.hmp.txt，只需提供前面的部分。
-4. 生成重复材料列表。
+4. 生成重复材料列表，并计算重复性，计算公式为$r=v/v_{a}$，这里$r$表示重复性，$v$表示重复碱基数量，$v_a$表示总碱基数量，有缺失的数据不计算在内。
 5. 生成TASSEL可打开的HMP文件。
 
 ## 使用方法
@@ -32,7 +39,9 @@
 ```R
 fName <- "newMyGeno"   # 修改为自己的文件名称
 subtext <- ":.*|-.*"   # 缩短材料名，去除无用信息用，多个处理用|隔开
-source("https://aozhangchina.github.io/R/HMPDeleteDup/HMPDeleteDup.r")   # 加载程序文件，需要联网
+missingSign <- "N"   # 缺失值符号，有些HMP格式的文件中用字母N表示缺失
+chooseOne <- TRUE   # 默认TRUE，当两个材料测定两次时，保留一个材料；FALSE表示合并两次测定，但会消耗很长的时间。
+source("https://aozhangchina.github.io/R/HMPDeleteDup/HMPDeleteDupV2.r")   # 加载程序文件，需要联网
 ```
 
 国内：
@@ -40,6 +49,8 @@ source("https://aozhangchina.github.io/R/HMPDeleteDup/HMPDeleteDup.r")   # 加�
 ```R
 fName <- "newMyGeno"   # 修改为自己的文件名称
 subtext <- ":.*|-.*"   # 缩短材料名，去除无用信息用，多个处理用|隔开
-source("https://dataholdcn.cn/R/HMPDeleteDup/HMPDeleteDup.r")   # 加载程序文件，需要联网
+missingSign <- "N"   # 缺失值符号，有些HMP格式的文件中用字母N表示缺失
+chooseOne <- TRUE   # 默认TRUE，当两个材料测定两次时，保留一个材料；FALSE表示合并两次测定，但会消耗很长的时间。
+source("https://dataholdcn.cn/R/HMPDeleteDup/HMPDeleteDupV2.r")   # 加载程序文件，需要联网
 ```
 
---
+++ R/HMPDeleteDup/HMPDeleteDupV2.r
@@ -0,0 +1,86 @@
+if(!exists("fName")){fName <- "newMyGeno"}
+if(!exists("subtext")){subtext <- ":.*"}
+if(!exists("missingSign")){missingSign <- "N"}
+if(!exists("chooseOne")){chooseOne <- TRUE}
+tempC1 <- NULL
+source("https://dataholdcn.cn/R/GBIT/GBIT.R")
+integratingMarkers <- function(myVector){
+    myVector[which(myVector == missingSign)] <- NA
+  if (any(is.na(myVector))){
+    myVector <- myVector[-which(is.na(myVector))]
+  }
+
+  table_vector <- table(myVector)
+
+  if(length(table(table_vector)) > 0){
+    myVector <- names(sort(table_vector,decreasing=T))[1]
+  }else{
+    myVector <- "N"
+  }
+  return(myVector)
+}
+GBIT.setwd(choose.dir())
+myGeno <- GBIT.readFile(choose= T,header = T)
+
+myGenoHeaderClean <- GBIT.geno.simplifiedName(myGeno,subtext = subtext)
+
+myGenoHeaderTwo <- cbind(names(myGeno),names(myGenoHeaderClean))
+write.table(myGenoHeaderTwo,paste0("GBIT/",fName,".name.compare.csv"),sep=",",row.names=FALSE,col.names = FALSE,quote = FALSE)
+
+myGenoHeaderRep <- GBIT.geno.delDup(myGenoHeaderClean)
+
+names(myGeno) <- names(myGenoHeaderClean)
+
+vlist <- as.data.frame(cbind(myGenoHeaderRep,c(1:length(myGenoHeaderRep))))
+names(vlist) <- c("Names","GID")
+
+same <- function(ss){
+  if (ss[1]==ss[2]&(ss[1]!="N"|ss[2]!="N")){
+    ind <- TRUE
+  }else{
+    ind <- FALSE
+  }
+}
+noNA <- function(ss){
+  if(length(which(ss=="N"))==0){
+    ind <- TRUE
+  }else{
+    ind <- FALSE
+  }
+}
+
+res <- numeric()
+for (i in 1:length(unique(vlist$GID))){
+  local1 <- vlist[which(vlist$GID==unique(vlist$GID)[i])[1],1]
+  local2 <- vlist[which(vlist$GID==unique(vlist$GID)[i])[2],1]
+  a <- cbind(myGeno[,which(names(myGeno)==local1)],myGeno[,which(names(myGeno)==local2)])
+  b <- apply(a,1,same)
+  bc <- apply(a,1,noNA)
+  
+  res <- c(res,length(which(b==TRUE))/length(which(bc==TRUE)))
+}
+names(res) <- myGenoHeaderRep
+write.table(res,"clipboard",sep="\t",col.names=FALSE)
+cat("The two taxa accuracy has been exported in the clipboard.\n")
+
+if (chooseOne==TRUE){
+  index <- which(duplicated(names(myGenoHeaderClean)))
+  if (length(index)>0){
+  myGeno2 <- myGeno[,-index]
+  }
+}else{
+  index <- which(names(myGenoHeaderClean) %in% myGenoHeaderRep)
+  myGeno2 <- myGeno[,-index]
+  for (i in myGenoHeaderRep){
+  tempA <- which(names(myGeno) == i)
+  tempB <- as.matrix(cbind(myGeno[tempA]))
+  tempB <- as.data.frame(tempB)
+  NA_vector <- NULL
+
+  system.time(tempC<- apply(tempB,1,integratingMarkers))
+  tempC1 <- cbind(tempC1,tempC)
+  }
+  myGeno2 <- cbind(myGeno2,tempC1)
+}
+
+GBIT.writeHMP(myGeno2,fName)
