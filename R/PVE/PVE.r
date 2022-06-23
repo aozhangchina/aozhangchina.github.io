28acafeccd0cfe3bdabedab46cc10ae8de4a8fd2
aozhangchina <zhangao7@qq.com>
2022-06-23 22:37:58 +0800

    Add files via upload

--- R/PVE/PVE.r
+++ R/PVE/PVE.r
@@ -12,6 +12,26 @@ geno <- GBIT.readFile(header=T,fname = geno.info)
 pheno <- GBIT.readFile(header=T,fname = pheno.info)
 slist <- GBIT.readFile(header=F,fname = slist.info)
 
+GBIT.geno.delDup(geno)
+
+geno_name <- names(geno[12:ncol(geno)])
+pheno_name <- pheno[,1]
+
+ck <- GBIT.matching.test(geno_name,pheno_name)
+
+if (!ck){
+  geno_1 <- geno[,1:11]
+  geno_2 <- geno[,12:ncol(geno)]
+  p_i <- which(names(pheno) %in% c("Genotype","GenoID","ID","Taxa"))
+  pheno_2 <- pheno[,p_i]
+  index <- intersect(names(geno_2),pheno_2)
+  geno_2 <- geno_2[,index]
+  pheno <- pheno[which(pheno[,p_i] %in% index),]
+  pheno <- pheno[order(pheno[,p_i]),]
+  geno_2 <- geno_2[,order(names(geno_2))]
+  geno  <- cbind(geno_1,geno_2)
+}
+
 myPVE <- GBIT.get.PVE(geno,pheno,slist)
 cat("The results of PVEs are in your clipboard.\n")
 write.table(myPVE,"GBIT\\PVE.txt",col.names=FALSE,sep="\t",quote=FALSE)
